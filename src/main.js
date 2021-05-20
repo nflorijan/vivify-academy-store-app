5b5b66333e7845f25747436c8962b8a4f8f8a4bc
nflorijan <florijan.nadj@gmail.com>
2021-05-20 21:33:35 +0200

    Zadatak 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import BootstrapVue from 'bootstrap-vue'
+import router from './router'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
